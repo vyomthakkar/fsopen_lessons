df4559ec972733d87478edfdf7a7f2c8c3809374
vyomthakkar <vyom.thakkar3508@gmail.com>
2021-12-31 10:29:05 +0530

    part 1 d basic first

---
+++ part1/left-right/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)
+
+  return (
+    <div>
+      {left}
+      <button onClick={() => setLeft(left+1)}>
+      left
+      </button>
+      {right}
+      <button onClick={() => setRight(right+1)}>
+      right
+      </button>
+    </div>
+  )
+}
+
+export default App
