df302435aad757cca9f7e1a341a717f4ca8127f5
vyomthakkar <vyom.thakkar3508@gmail.com>
2021-12-31 10:54:48 +0530

    part 1d allclicks

--- part1/left-right/src/App.js
+++ part1/left-right/src/App.js
@@ -3,17 +3,29 @@ import React, { useState } from 'react'
 const App = () => {
   const [ left, setLeft ] = useState(0)
   const [ right, setRight ] = useState(0)
+  const [ allClicks, setAll ] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
+  }
 
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left+1)}>
+      <button onClick={handleLeftClick}>
       left
       </button>
       {right}
-      <button onClick={() => setRight(right+1)}>
+      <button onClick={handleRightClick}>
       right
       </button>
+      <p> {allClicks.join(' ')} </p>
     </div>
   )
 }
