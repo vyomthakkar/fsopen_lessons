c87254e2dc8ed28c020d1f6e7237b58e7601dd03
vyomthakkar <vyom.thakkar3508@gmail.com>
2022-01-25 09:40:56 -0600

    before making service requests change

--- part2/part2/src/App.js
+++ part2/part2/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -46,6 +47,19 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+  const toggleImportanceOf = (id) => {
+    console.log("toggle importance of ",id)
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios
+    .put(url,changedNote)
+    .then(response => {
+      setNotes(notes.map(note => note.id === id ? response.data : note))
+    })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -56,7 +70,7 @@ const App = () => {
       </div>
       <ul>
       {notesToShow.map(note =>
-        <Note key={note.id} note={note} />
+        <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
       )}
       </ul>
       <form onSubmit={addNote}>
