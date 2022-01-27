3e3ae480b37e89ce9b8f9222f480d80c54038aab
vyomthakkar <vyom.thakkar3508@gmail.com>
2022-01-27 11:49:17 -0600

    e inline styles

--- part2/part2/src/App.js
+++ part2/part2/src/App.js
@@ -5,6 +5,20 @@ import Notification from './components/Notification'
 import noteService from './services/notes'
 import './index.css'
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
@@ -86,6 +100,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type='submit'>save</button>
       </form>
+      <Footer />
     </div>
   )
 }
